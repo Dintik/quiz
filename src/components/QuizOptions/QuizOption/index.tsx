2963c622e30ac8b81c5221a3d4b195e65d67dcfa
Dintik <chailo.artem@gmail.com>
2025-03-15 03:49:40 +0200

    add checkbox to multiple-select QuizOption

--- src/components/QuizOptions/QuizOption/index.tsx
+++ src/components/QuizOptions/QuizOption/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Option, QuestionType } from '@/types/quiz'
+import CheckboxIcon from '@/assets/images/Checkbox.svg'
 import styles from './styles.module.scss'
 
 export interface QuizOptionProps {
@@ -14,22 +15,33 @@ export const QuizOption = ({
   type,
   selectedOption,
   onClick
-}: QuizOptionProps) => (
-  <button
-    className={`${styles.option} ${styles[`option__${type}`]} ${selectedOption && styles.option__selected}`}
-    onClick={onClick}
-  >
-    {option.image && (
-      <div className={styles.option__image_container}>
-        <Image
-          src={option.image}
-          alt={option.text}
-          className={styles.option__image}
-          width={type === 'bubble' ? 25 : 52}
-          height={type === 'bubble' ? 25 : 52}
-        />
-      </div>
-    )}
-    <span>{option.text}</span>
-  </button>
-)
+}: QuizOptionProps) => {
+  const isMultiSelect = type === 'multiple-select'
+
+  return (
+    <button
+      className={`${styles.option} ${styles[`option__${type}`]} ${selectedOption && styles.option__selected}`}
+      onClick={onClick}
+    >
+      {option.image && (
+        <div className={styles.option__image_container}>
+          <Image
+            src={option.image}
+            alt={option.text}
+            className={styles.option__image}
+            width={type === 'bubble' ? 25 : 52}
+            height={type === 'bubble' ? 25 : 52}
+          />
+        </div>
+      )}
+      <span>{option.text}</span>
+      {isMultiSelect && (
+        <div
+          className={`${styles.option__checkbox} ${selectedOption && styles.option__checkbox_selected}`}
+        >
+          <CheckboxIcon />
+        </div>
+      )}
+    </button>
+  )
+}
