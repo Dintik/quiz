b2d726184c5aa108be5db2cec9c706ba19dbd4fe
Dintik <chailo.artem@gmail.com>
2025-03-15 19:42:03 +0200

    add ResultPage layout

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,28 +1,39 @@
 'use client'
-import { Button } from '@/components/ui/Button'
 import { useTranslations } from 'next-intl'
-import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from '@/components/ui/Button'
+import Download from '@/assets/images/Download.png'
+import Checkmark from '@/assets/images/Checkmark.png'
+import styles from './styles.module.scss'
 
 export default function ResultPage() {
   const t = useTranslations()
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1>{t('result.title')}</h1>
-          <p>{t('result.description')}</p>
+    <div className={styles.result}>
+      <div className={styles.result__content}>
+        <div className={styles.result__header}>
+          <h1 className={styles.result__title}>{t('result.title')}</h1>
+          <p className={styles.result__desc}>{t('result.description')}</p>
         </div>
 
-        {/* TODO: check-icon */}
+        <div>
+          <Image
+            src={Checkmark}
+            alt='success checkmark'
+            width={118}
+            height={118}
+          />
+        </div>
       </div>
-
-      <button onClick={() => null}>{t('result.download')}</button>
-      <Link href={'/quiz/1'}>{t('result.retake')}</Link>
-      <Button onClick={() => null}>{t('result.retake')}</Button>
-      <Button isLoading onClick={() => null}>
-        {t('result.retake')}
-      </Button>
+      <div className={styles.result__buttons}>
+        <button className={styles.result__download} onClick={() => null}>
+          <Image src={Download} alt='download icon' width={42} height={42} />
+          {t('result.download')}
+        </button>
+        {/* <Link href={'/quiz/1'}>{t('result.retake')}</Link> */}
+        <Button onClick={() => null}>{t('result.retake')}</Button>
+      </div>
     </div>
   )
 }
